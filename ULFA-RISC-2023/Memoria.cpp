7a439e4de9c39bbb34f0e1ea5dcf0a33a1331c2e
brunof5 <101609538+brunof5@users.noreply.github.com>
2023-05-20 16:50:42 -0300

    Adicionando memoria e teste simples

--- ULFA-RISC-2023/Memoria.cpp
+++ ULFA-RISC-2023/Memoria.cpp
@@ -1,19 +1,37 @@
+#include <bitset>
+
 class Memoria {
 
     private:
-        int teste;
+        std::bitset<dataBus> *mem;
+        int tamanhoMemoria;
 
     public:
         Memoria();
         ~Memoria();
+        void testarMemoria();
 };
 
 Memoria::Memoria() {
 
+    tamanhoMemoria = pow(2, adressBus);
 
+    mem = new std::bitset<dataBus>[tamanhoMemoria];
+
+    testarMemoria();
 }
 
 Memoria::~Memoria() {
 
+    delete[] mem;
+}
 
+void Memoria::testarMemoria() {
+
+    mem[0].set(2);
+    mem[65535].set(4);
+
+    cout << mem[0] << endl;
+    cout << mem[1] << endl;
+    cout << mem[65535] << endl;
 }
