aacceeb0506ab2a8b8b72ea16c9b9df057d9d8a4
Bruno C <bruno.ferreira5@estudante.ufla.br>
2023-05-22 21:35:12 -0300

    Reformulação do If.cpp e Processador.cpp

--- codes/If.cpp
+++ codes/If.cpp
@@ -1,25 +1,20 @@
 class If {
 
     private:
-        string registradorInstrucoes;
         int PC;
-        bool addressLido;
-        int linhaAtual;
-        bool encontrarAddress();
-        int conversorBinario(char bin[], int tamVet);
+        bitset<dataBus> registradorInstrucoes;
+        Memoria *memoria;
 
     public:
-        If();
+        If(Memoria *memoria, int PC);
         ~If();
-        void executar(string input);
-        string getInstrucao();
+        bitset<dataBus> getInstrucao();
 };
 
-If::If() {
+If::If(Memoria *memoria, int PC) {
 
-    PC = 0;
-    addressLido = false;
-    linhaAtual = 0;
+    this->memoria = memoria;
+    this->PC = PC;
 }
 
 If::~If() {
@@ -27,90 +22,10 @@ If::~If() {
 
 }
 
-string If::getInstrucao(){
+bitset<dataBus> If::getInstrucao(){
+
+    registradorInstrucoes = memoria->getInstrucao(PC);
+    PC++;
 
     return registradorInstrucoes;
-}
-
-void If::executar(string input) {
-
-    ifstream arquivo_entrada("files/" + input + ".txt");
-
-    if(arquivo_entrada) {
-
-        // "abre" o arquivo em uma linha especifica
-        for(int i = 0; i < linhaAtual; i++) {
-
-            string lixo;
-
-            getline(arquivo_entrada, lixo);
-        }
-
-        // leitura uma instrucao (ou de um "address")
-		getline(arquivo_entrada, registradorInstrucoes);
-        linhaAtual++;
-
-        if(!addressLido) {
-
-            addressLido = encontrarAddress();
-        }
-
-        arquivo_entrada.close();
-    }
-}
-
-bool If::encontrarAddress() {
-
-    // tenta achar se "address" foi escrito
-    char espaco = ' ';
-	size_t found_esp = registradorInstrucoes.find(espaco);
-
-    // achou
-	if(found_esp != string::npos){
-        
-        // encontra-se o tamanho do binario do endereco da memoria de texto
-        int tamanhoBinEnderecoMemoriaTexto = registradorInstrucoes.size() - found_esp - 1;
-        char enderecoMemoriaTexto_Char[tamanhoBinEnderecoMemoriaTexto];
-
-        // enderecoMemoriaTexto_Char recebe a string apenas com o binario
-        for(int i = 0; i < tamanhoBinEnderecoMemoriaTexto; i++) {
-
-            enderecoMemoriaTexto_Char[i] = registradorInstrucoes[found_esp + 1 + i];
-        }
-
-        int aux = conversorBinario(enderecoMemoriaTexto_Char, tamanhoBinEnderecoMemoriaTexto);
-
-        if(aux >= pow(2, adressBus) or aux < 0) {
-
-            // FAZER ERRO
-        }
-
-        else {
-
-            PC = aux;
-
-            return true;
-        }
-	}
-
-    return false;
-}
-
-// converte o endereco binario para int
-int If::conversorBinario(char bin[], int tamVet) {
-
-    int retorno = 0;
-	int potencia_dois = 1;
-	
-	for(int i = tamVet; i > 0; i--){
-		
-		if(bin[i - 1] == '1') {
-
-            retorno += potencia_dois;
-        }
-
-		potencia_dois *= 2;
-	}
-	
-	return retorno;
 }
--- codes/Processador.cpp
+++ codes/Processador.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <math.h>
 #include <fstream>
 #include <bitset>
@@ -7,26 +8,25 @@
 
 using namespace std;
 
-#include "Alu.cpp"
-#include "ExMem.cpp"
-#include "Id.cpp"
-#include "If.cpp"
 #include "Memoria.cpp"
-#include "Registradores.cpp"
+#include "If.cpp"
+#include "Id.cpp"
+#include "ExMem.cpp"
 #include "Wb.cpp"
-#include "Controle.cpp"
 
 class Processador {
 
     private:
-        Alu *alu;
-        ExMem *exMemStage;
-        Id *idStage;
-        If *ifStage;
         Memoria *memoria;
-        Registradores *registrador;
-        Wb *wbStage;
-        Controle *controle;
+        If *ifStage;
+        //Id *idStage;
+        //ExMem *exMemStage;
+        //Wb *wbStage;
+        int qtdClocks;
+        int leituraArquivoEntrada();
+        bool encontrarAddress(string instrucaoAtual, int &enderecoComemoMemoriaTexto);
+        int conversorBinario(char bin[], int tamVet);
+        void incrementarClock();
 
     public:
         Processador();
@@ -36,40 +36,149 @@ class Processador {
 
 Processador::Processador() {
 
-    alu = new Alu();
-    exMemStage = new ExMem();
-    idStage = new Id();
-    ifStage = new If();
     memoria = new Memoria();
-    registrador = new Registradores();
-    wbStage = new Wb();
-    controle = new Controle();
+    //idStage = new Id();
+    //exMemStage = new ExMem();
+    //wbStage = new Wb();
+
+    qtdClocks = 0;
 }
 
 Processador::~Processador() {
 
-    delete alu;
-    delete exMemStage;
-    delete idStage;
-    delete ifStage;
     delete memoria;
-    delete registrador;
-    delete wbStage;
-    delete controle;
+    delete ifStage;
+    //delete idStage;
+    //delete exMemStage;
+    //delete wbStage;
 }
 
 void Processador::executar() {
+
+    int PC = leituraArquivoEntrada();
+
+    ifStage = new If(memoria, PC);
+
+    cout << ifStage->getInstrucao() << endl;
+    incrementarClock();
+    cout << ifStage->getInstrucao() << endl;
+    incrementarClock();
+
+    cout << qtdClocks << endl;
+}
+
+int Processador::leituraArquivoEntrada() {
 
     string nome_arquivo_entrada;
 
     cout << "Digite o nome do arquivo de entrada de instruções (sem extensão): ";
 	cin >> nome_arquivo_entrada;
 
-    do {
+    ifstream arquivo_entrada("files/" + nome_arquivo_entrada + ".txt");
 
-        ifStage->executar(nome_arquivo_entrada);
+    if(arquivo_entrada) {
 
-        cout << ifStage->getInstrucao() << endl;
+        string instrucaoAtual;
+        bool addressLido = false, primeiraLeitura = false;
+        int enderecoComecoMemoriaTexto = 0, enderecoMemoriaTexto = enderecoComecoMemoriaTexto;
 
-    } while(ifStage->getInstrucao() != "11111111111111111111111111111111");
+        do {
+
+            // leitura uma instrucao (ou de um "address")
+		    getline(arquivo_entrada, instrucaoAtual);
+
+            if(!primeiraLeitura and !addressLido) {
+
+                primeiraLeitura = true;
+
+                addressLido = encontrarAddress(instrucaoAtual, enderecoComecoMemoriaTexto);
+
+                enderecoMemoriaTexto = enderecoComecoMemoriaTexto;
+
+                if(addressLido){
+
+                    continue;
+                }
+            }
+
+            // registro da instrucao na memoria
+            memoria->armazenarInstrucao(instrucaoAtual, enderecoMemoriaTexto);
+            enderecoMemoriaTexto++;
+
+        } while(instrucaoAtual != "11111111111111111111111111111111");
+
+        arquivo_entrada.close();
+
+        return enderecoComecoMemoriaTexto;
+    }
+
+    else {
+
+        // FAZER ERRO
+
+        return -1;
+    }
+}
+
+// tenta achar se "address" foi escrito
+bool Processador::encontrarAddress(string instrucaoAtual, int &enderecoComemoMemoriaTexto) {
+
+    char espaco = ' ';
+	size_t found_esp = instrucaoAtual.find(espaco);
+
+    // achou
+	if(found_esp != string::npos){
+        
+        // encontra-se o tamanho do binario do endereco da memoria de texto
+        int tamanhoBinEnderecoMemoriaTexto = instrucaoAtual.size() - found_esp - 1;
+        char enderecoMemoriaTexto_Char[tamanhoBinEnderecoMemoriaTexto];
+
+        // enderecoMemoriaTexto_Char recebe a string apenas com o binario
+        for(int i = 0; i < tamanhoBinEnderecoMemoriaTexto; i++) {
+
+            enderecoMemoriaTexto_Char[i] = instrucaoAtual[found_esp + 1 + i];
+        }
+
+        int aux = conversorBinario(enderecoMemoriaTexto_Char, tamanhoBinEnderecoMemoriaTexto);
+
+        if(aux >= pow(2, adressBus) or aux < 0) {
+
+            // FAZER ERRO
+
+            return false;
+        }
+
+        else {
+
+            enderecoComemoMemoriaTexto = aux;
+
+            return true;
+        }
+	}
+
+    return false;
+}
+
+// converte o endereco binario para int
+int Processador::conversorBinario(char bin[], int tamVet) {
+
+    int retorno = 0;
+	int potencia_dois = 1;
+	
+	for(int i = tamVet; i > 0; i--){
+		
+		if(bin[i - 1] == '1') {
+
+            retorno += potencia_dois;
+        }
+
+		potencia_dois *= 2;
+	}
+
+    return retorno;
+}
+
+void Processador::incrementarClock() {
+
+    qtdClocks++;
 }
